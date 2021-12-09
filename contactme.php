0bfe038837b745cb543c52ad1cbe680ed096fea8
RashiDixit27 <89250349+RashiDixit27@users.noreply.github.com>
2021-12-09 13:40:56 +0530

    Delete contactme.php

--- contactme.php
+++
@@ -1,9 +0,0 @@
-<?php $name = $_POST['ipt1'];
-$email = $_POST['ipt2'];
-$message = $_POST['ipt3'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "rashimdixit@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-?>
